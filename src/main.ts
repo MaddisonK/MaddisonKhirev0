8d2453d87e4cca19795f25c623ba3226615887f5
MaddisonK <maddison.khire@gmail.com>
2023-05-30 21:28:07 +1200

    installed bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,6 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-import $ from 'jquery';
+// Import our custom CSS
+import '/styles.scss';
 
-$('div#app').get()[0]!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
